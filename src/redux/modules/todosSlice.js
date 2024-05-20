24eb0889bdf24920516bbbe69ecde25e1b5def71
yeye921 <yewon921@ajou.ac.kr>
2024-05-20 11:58:27 +0900

    feat: 투두리스트 기능 추가

---
+++ src/redux/modules/todosSlice.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+/**
+ * 2초 지연을 시키는 함수입니다 (비동기 작업).
+ */
+import { waitTwoSeconds } from '../../utils';
+
+export const __addToDo = createAsyncThunk(
+  '__addToDo',
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    thunkAPI.dispatch(addTodo(payload));
+  }
+);
+
+export const __deleteTodo = createAsyncThunk(
+  '__deleteToDo',
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    thunkAPI.dispatch(deleteTodo(payload));
+  }
+);
+
+const initialState = {
+  list: [],
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.list.push(action.payload);
+    },
+    deleteTodo: (state, action) => {
+      state.list = state.list.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer;
